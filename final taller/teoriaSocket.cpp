93ad25d8b6d9cefeea2363cc18e77b18591b437f
FacuGerez <facundogerezuba@gmail.com>
2025-03-02 15:55:50 -0300

    socket exercise until 2pdf 2019.1

--- final taller/teoriaSocket.cpp
+++ final taller/teoriaSocket.cpp
@@ -143,3 +143,129 @@ int main(int argc, char const *argv[]){
     return 0;
 }
 */
+
+// Ejercicios:
+
+// Escriba un programa que reciba por línea de comandos un Puerto y una IP. El programa debe
+// establecer una unica conexión, quedar en escucha e imprimir en stdout todo lo recibido. Al recibir
+// el texto ‘FINAL’ debe finalizar el programa sin imprimir dicho texto.
+
+/*
+int main(int argc, char const *argv[])
+{
+    if (argc != 3) return -1;
+    char *puerto = argv[1];
+    char *ip = argv[2];
+
+    struct addrinfo hints{}, *addr;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = 0;
+
+    if (getaddrinfo(ip, puerto, &hints, &addr) != 0) return -1;
+
+    int skt = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+    if (skt == -1){
+        freeaddrinfo(addr);
+        return -1;
+    }
+
+    if (connect(skt, addr->ai_addr, addr->ai_addrlen) == -1){
+        freeaddrinfo(addr);
+        close(skt);
+        return -1;
+    }
+
+    freeaddrinfo(addr);
+
+    char byteorcaracter;
+    while (recv(skt, &byteorcaracter, sizeof(char), 0) > 0){
+        if (byteorcaracter == 'F'){
+            char buffer[5];
+            recv(skt, buffer, 4, 0);
+            buffer[4] = '\0';
+            if (strcmp(buffer, "INAL") == 0) {
+                break;
+            }else{
+                std::cout << byteorcaracter << buffer;
+            }
+        }else{
+            std::cout << byteorcaracter;
+        }
+    }
+}
+*/
+
+
+// Escriba un programa (desde la inicialización hasta la liberación de los recursos) que reciba
+// paquetes   de   la   forma   nnn+nn+....+nnnn=  (numeros separados por +, seguidos de =) e
+// imprima el resultado de la suma de cada paquete por pantalla. Al recibir un paquete vacío
+// (“=”)  debe cerrarse ordenadamente. No considere errores.
+
+/*
+int main(){
+    char *puerto = "8080";
+    char *ip = "127.0.0.1";
+
+    struct addrinfo hints{}, *addr;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+
+    if (getaddrinfo(ip, puerto, &hints, &addr) != 0) return -1;
+
+    int skt = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+
+    if (skt == -1){
+        freeaddrinfo(addr);
+        return -1;
+    }
+
+    if (bind(skt, addr->ai_addr, addr->ai_addrlen) == -1){
+        freeaddrinfo(addr);
+        close(skt);
+        return -1;
+    }
+
+    freeaddrinfo(addr);
+
+    if (listen(skt, 1) == -1){
+        close(skt);
+        return -1;
+    }
+
+    int skt_c = accept(skt, NULL, NULL);
+    if (skt_c == -1){
+        close(skt);
+        return -1;
+    }
+
+    char byteorcaracter;
+    std::string numero = "";
+    char anterior = '=';
+    int suma = 0;
+    while (recv(skt_c, &byteorcaracter, sizeof(char), 0) > 0){
+        if (byteorcaracter == '+'){
+            suma += std::stoi(numero);
+            numero = "";
+        }else if (byteorcaracter == '='){
+            if (anterior == '='){
+                break;
+            }
+            suma += std::stoi(numero);
+            std::cout << suma << std::endl;
+            suma = 0;
+            numero = "";
+        }else{
+            numero += byteorcaracter;
+        }
+        anterior = byteorcaracter;
+    }
+
+    shutdown(skt_c, SHUT_RDWR);
+    close(skt_c);
+    shutdown(skt, SHUT_RDWR);
+    close(skt);
+    return 0;
+}
+*/
